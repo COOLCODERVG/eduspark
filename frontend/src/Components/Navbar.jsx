ce5d9c9a79dc2efef6c532b565a98b74d290ac89
Varshith Gude <varshith.gudeus@gmail.com>
2025-06-28 22:40:36 -0700

    idk will ts work

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -180,10 +180,26 @@ function Navbar() {
             </li>
             <li>
               <a
-                href="/login"
+                href="/soon/"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
-                Login
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href="/soon/"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Programs
+              </a>
+            </li>
+            <li>
+              <a
+                href="/soon/"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Membership
               </a>
             </li>
           </ul>
